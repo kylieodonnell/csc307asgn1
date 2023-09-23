600a39c87d703ae4f3faac6e3597a2a0ee85c6f2
Kylie O'Donnell <knodonne@calpoly.edu>
2023-09-22 19:04:14 -0700

    2nd commit

--- src/Table.js
+++ src/Table.js
@@ -17,9 +17,15 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
-      </tr>
+      <td>{row.name}</td>
+      <td>{row.job}</td>
+      <td>
+        <button onClick={() => 
+          props.removeCharacter(index)}>
+          Delete
+        </button>
+      </td>
+    </tr>
     );
    }
   );
@@ -30,13 +36,14 @@ function TableBody(props) {
    );
 }
 
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} 
+	      removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 export default Table;
